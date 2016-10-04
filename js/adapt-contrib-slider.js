e741a89d7ab90e59e5a1d01ffbb2cd3114e421d3
Oliver Foster <oliver.foster@kineo.com>
2016-10-04 12:02:23 +0100

    issue-1228: moved libraries out

--- js/adapt-contrib-slider.js
+++ js/adapt-contrib-slider.js
@@ -1,7 +1,7 @@
 define([
   'coreViews/questionView',
   'coreJS/adapt',
-  './rangeslider.js'
+  'libraries/rangeslider.js'
 ], function(QuestionView, Adapt, Rangeslider) {
 
     var Slider = QuestionView.extend({
