9aebf5a30e223cf3773b98b82a8d1b7cd8495f49
Oliver Foster <oliver.foster@kineo.com>
2016-10-04 12:08:59 +0100

    issue-1228: removed .js from require

--- js/adapt-contrib-slider.js
+++ js/adapt-contrib-slider.js
@@ -1,7 +1,7 @@
 define([
   'coreViews/questionView',
   'coreJS/adapt',
-  'libraries/rangeslider.js'
+  'libraries/rangeslider'
 ], function(QuestionView, Adapt, Rangeslider) {
 
     var Slider = QuestionView.extend({
