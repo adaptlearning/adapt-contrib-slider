4e21470ecd41d4eb114c4b07ed03bcc9163c3879
Lemmy Adams <103187526+lemmyadams@users.noreply.github.com>
2024-04-11 14:39:19 +0100

    Chore: e2e tests on slider component (Issue/201) (#202)

---
+++ test/e2e/slider.cy.js
@@ -0,0 +1,24 @@
+describe('Slider', function () {
+  beforeEach(function () {
+    cy.getData();
+    cy.visit('/');
+  });
+
+  it('should display the slider component', function () {
+    const sliderComponents = this.data.components.filter(component => component._component === 'slider');
+    sliderComponents.forEach(sliderComponent => {
+      cy.visit(`/#/preview/${sliderComponent._id}`);
+      const stripHtml = cy.helpers.stripHtml;
+      cy.testContainsOrNotExists('.slider__body', stripHtml(sliderComponent.body));
+      cy.testContainsOrNotExists('.slider__title', stripHtml(sliderComponent.displayTitle));
+      cy.testContainsOrNotExists('.slider__instruction', stripHtml(sliderComponent.instruction));
+
+      cy.get('.slider__number').should('contain', sliderComponent._scaleStart);
+      cy.get('.slider__number').should('contain', sliderComponent._scaleEnd);
+
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      cy.wait(1000);
+    });
+  });
+});
